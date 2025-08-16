ed88612e1c08a660716a8a48ab388f5339a5224c
WangYihanParrot <wangyihandw@gmail.com>
2025-08-16 20:24:09 +0800

    把html编译进文件,修改编译文件和说明文件

--- main.go
+++ main.go
@@ -1,32 +1,29 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"log"
 	"net/http"
-	"os"
 )
+
+//go:embed clock.html
+var clockHTML []byte
 
 func main() {
 	port := flag.String("port", "4000", "监听端口号")
 	flag.Parse()
 
-	content, err := os.ReadFile("clock.html")
-	if err != nil {
-		log.Fatalf("读取文件失败: %v\n", err)
-	}
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		if _, err := w.Write(content); err != nil {
+		if _, err := w.Write(clockHTML); err != nil {
 			log.Printf("写入响应失败: %v\n", err)
 		}
 	})
 
 	addr := ":" + *port
 	log.Printf("启动 HTTP 服务器，监听 http://127.0.0.1%s\n", addr)
-	err = http.ListenAndServe(addr, nil)
-	if err != nil {
+	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatalf("HTTP 服务器启动失败: %v\n", err)
 	}
 }
